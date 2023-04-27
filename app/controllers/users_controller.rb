a47c65270dae5f856d7d60242a73206e6cb115d2
Finch Yuan <fyuan@chicagobooth.edu>
2023-04-27 17:21:36 +0000

    Finished users. Form pending

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,18 @@ class UsersController < ActionController::Base
     
     render ({ :template => "user_template/index.html.erb"})
   end
+
+  def detail 
+    # Parameters: {"username"=>"anisa"}
+    input_username = params.fetch("username")
+    matching_username = User.where({:username=> input_username})
+    @user = matching_username.at(0)
+
+    if @user == nil
+      redirect_to("/404")
+    else
+      render ({ :template => "user_template/detail.html.erb"})
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get("/", {:controller => "users", :action => "index"})
 
   get("/users", {:controller => "users", :action => "index"})
+
+  get("/users/:username", {:controller => "users", :action => "detail"})
 
 end
